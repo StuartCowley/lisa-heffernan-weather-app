a60c52c4202761aa044d25b412379dd480f2e071
Lisa Heffernan <lisaheffernan@outlook.com>
2022-09-08 13:14:29 +0100

    component location details created and renders city and country

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/App.css";
+import LocationDetails from "./LocationDetails";
+
+function App({ location }) {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
