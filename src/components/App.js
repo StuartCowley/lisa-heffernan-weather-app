c03a1f014508bdcc260b8664a1cc8bb1d48ad819
Lisa Heffernan <lisaheffernan@outlook.com>
2022-09-16 11:50:43 +0100

    replaced static data from json file with live data using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-/* eslint-disable  no-unused-vars */
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import getForecast from "../requests/getForecast";
 
 import "../styles/App.css";
 
-const App = ({ location, forecasts }) => {
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
-  const selectedForecast = forecasts.find((forecast) => forecast.date === selectedDate);
-  const { city, country } = location;
+const App = () => {
+  const [forecasts, setForecasts] = useState([]);
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [selectedDate, setSelectedDate] = useState(0);
+
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate,
+  );
+
+  useEffect(() => {
+    getForecast(setSelectedDate, setForecasts, setLocation);
+  }, []);
 
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
@@ -18,29 +25,14 @@ const App = ({ location, forecasts }) => {
 
   return (
     <div className="weather-app">
-      <LocationDetails city={city} country={country} />
-      <ForecastSummaries forecasts={forecasts} onForecastSelect={handleForecastSelect} />
-      <ForecastDetails forecast={selectedForecast} />
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries
+        forecasts={forecasts}
+        onForecastSelect={handleForecastSelect}
+      />
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
   );
-};
-
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number,
-      description: PropTypes.string,
-      icon: PropTypes.string,
-      temperature: PropTypes.shape({
-        max: PropTypes.number,
-        min: PropTypes.number,
-      }),
-    }),
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
 };
 
 export default App;
