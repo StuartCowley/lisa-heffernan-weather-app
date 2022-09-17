b4901500865df2c4945955cf81450422554809d6
Lisa Heffernan <lisaheffernan@outlook.com>
2022-09-17 18:21:36 +0100

    another file to see if git will push

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "../styles/ForecastDetails.css";
+
 const ForecastDetails = ({ forecast }) => {
   const {
     date, temperature, humidity, wind,
