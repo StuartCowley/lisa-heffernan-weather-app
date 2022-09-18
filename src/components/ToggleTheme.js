f301b6ecbd3b25ff437ce9150f379a8995f45ef8
Lisa Heffernan <lisaheffernan@outlook.com>
2022-09-18 12:48:37 +0100

    dark theme style added to theme button

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { useThemeContext } from "../contexts/ThemeContext";
+import "../styles/ToggleTheme.css";
 
 const ToggleTheme = () => {
-  const { setUseDarkTheme } = useThemeContext();
+  const { useDarkTheme, setUseDarkTheme } = useThemeContext();
 
   const handleClick = () => {
     setUseDarkTheme((prev) => !prev);
   };
 
   return (
-    <button type="submit" onClick={handleClick}>toggle theme</button>
+    <div className="theme-button">
+      <button className={`theme-button__button ${useDarkTheme ? "dark" : "light"}`} type="submit" onClick={handleClick}>toggle theme</button>
+    </div>
   );
 };
 
